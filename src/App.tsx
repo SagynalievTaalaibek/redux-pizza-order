2af8c8eb0bb0b0539ddb1e1ae8dbbe56d1969415
SagynalievTaalaibek <sagynalievv.t@gmail.com>
2024-01-21 20:32:28 +0600

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
+
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <Layout>
+      <Routes>
+        <Route path="/" element={'Home'}/>
+        <Route path="/admin/dishes" element={'Dishes'}/>
+        <Route path="/admin/orders" element={'Orders'}/>
+        <Route path="/admin/new-dish" element={'New Dish Form'}/>
+        <Route path="/admin/edit-dish/:id" element={'Edit Dish Form'}/>
+        <Route path="*" element={'Not found'}/>
+      </Routes>
+    </Layout>
   );
 };
 
